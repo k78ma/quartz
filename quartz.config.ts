58f3bcbd6be9212bdddad36103386f99cf18b2c4
Kai Ma <k78ma@uwaterloo.ca>
2023-08-26 00:20:07 -0700

    dumbass mistake

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     baseUrl: "k78ma.github.io/quartz",
-    ignorePatterns: ["private", "templates", "**/private", "**/templates", "**/Private", "*.md"],
+    ignorePatterns: ["private", "templates", "**/private", "**/templates", "**/Private", "*.excalidraw"],
     defaultDateType: "created",
     theme: {
       typography: {
